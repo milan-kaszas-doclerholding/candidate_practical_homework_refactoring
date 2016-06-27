718142e9085676ddccaffe930430556c4beae273
Vasil Vanchuk <v.vanchuk@tut.by>
2016-06-27 19:00:23 +0300

    Add tests for 'protected static checkForApiErrorResult'

--- tests/LanguageBatchBoTest.php
+++ tests/LanguageBatchBoTest.php
@@ -3,6 +3,19 @@
 chdir(__DIR__);
 include('../vendor/autoload.php');
 
+/**
+ * Allow test protected methods
+ * No need in real, but it will be helpful on existing code,
+ * cause we have no tests on init
+ * In real code it's antiPattern `Paulik Morozov'
+ * Class LanguageBatchBoChild
+ */
+class LanguageBatchBoChild extends \Language\LanguageBatchBo {
+    public static function checkForApiErrorResult($result) {
+        return forward_static_call(array('\Language\LanguageBatchBo', 'checkForApiErrorResult'), $result);
+    }
+};
+
 class LanguageBatchBoTest extends PHPUnit_Framework_TestCase
 {
 
@@ -33,5 +46,44 @@ Getting applet language XMLs..
 
 Applet language XMLs generated.
 ', $output, 'The output is the same as at start');
+    }
+    
+    public function testCheckForApiErrorResultWhileErrorDuringApiCall()
+    {
+        try {
+            LanguageBatchBoChild::checkForApiErrorResult(false);
+            $this->fail('Should throw exception "Error during the api call"');
+        } catch (Exception $ex) {
+            $this->assertEquals($ex->getMessage(), "Error during the api call", "Error during the api call (false as result)");
+        }
+
+        try {
+            LanguageBatchBoChild::checkForApiErrorResult([]);
+            $this->fail('Should throw exception "Error during the api call"');
+        } catch (Exception $ex) {
+            $this->assertEquals($ex->getMessage(), "Error during the api call", "Error during the api call (no status in result");
+        }
+    }
+
+
+    public function testCheckForApiErrorWhileWrongResponse()
+    {
+        try {
+            LanguageBatchBoChild::checkForApiErrorResult(['status' => 'wrong', 'data' => true ]);
+            $this->fail('Should throw exception "Wrong response"');
+        } catch (Exception $ex) {
+            $this->assertContains("Wrong response", $ex->getMessage(), "Wrong response should throw exception");
+        }
+    }
+
+
+    public function testCheckForApiErrorWhileWrongContent()
+    {
+        try {
+            LanguageBatchBoChild::checkForApiErrorResult(['status' => 'OK', 'data' => false ]);
+            $this->fail('Should throw exception "Wrong content"');
+        } catch (Exception $ex) {
+            $this->assertContains("Wrong content", $ex->getMessage(), "Wrong response should throw exception");
+        }
     }
 }
